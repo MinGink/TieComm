d5ab9d858c3c7730b53ed5a115c5bf046fdaa3b1
ming <yangmingink@gmail.com>
2022-07-28 13:26:58 +0800

    ready

--- runner/runner_one.py
+++ runner/runner_one.py
@@ -19,9 +19,9 @@ class RunnerOne(Runner):
         self.onegroup = [list(range(self.n_agents))]
         self.no_group = [[i] for i in range(self.n_agents)]
 
-        self.params = [p for p in self.agent.agent.parameters()]
-        # self.optimizer = Adam(params=self.agent.parameters(), lr=self.args.lr)
-        self.optimizer = RMSprop(self.agent.agent.parameters(), lr=self.args.lr, alpha=0.97, eps=1e-6)
+        # self.params = [p for p in self.agent.agent.parameters()]
+        # # self.optimizer = Adam(params=self.agent.parameters(), lr=self.args.lr)
+        # self.optimizer = RMSprop(self.agent.agent.parameters(), lr=self.args.lr, alpha=0.97, eps=1e-6)
 
 
     def run_an_episode(self):
